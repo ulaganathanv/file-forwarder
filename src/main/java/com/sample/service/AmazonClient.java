d60fb523baa98125072dd70ee4e2f5185d2abc00
Ulaganathan Vallinayagam <ulag.india@gmail.com>
2020-06-16 13:50:19 +0530

    include download and delete operations

--- src/main/java/com/sample/service/AmazonClient.java
+++ src/main/java/com/sample/service/AmazonClient.java
@@ -1,24 +1,29 @@
 package com.sample.service;
 
 import java.util.List;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
 import javax.annotation.PostConstruct;
 
-import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 
-import com.amazonaws.SdkClientException;
-import com.amazonaws.AmazonServiceException;
-
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 
 @Service
 public class AmazonClient {
@@ -55,6 +60,46 @@ public class AmazonClient {
             // Amazon S3 couldn't be contacted for a response, or the client
             // couldn't parse the response from Amazon S3.
             e.printStackTrace();
+        }
+    }
+
+    public File downloadObject(String bucketName, String keyName) {
+
+        File file = new File(keyName);
+        try {
+            S3Object s3Object = s3client.getObject(bucketName, keyName);
+            S3ObjectInputStream s3ObjectInputStream = s3Object.getObjectContent();
+
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+            byte[] readBuffer = new byte[1024];
+            int readLength = 0;
+            while ((readLength = s3ObjectInputStream.read(readBuffer)) > 0) {
+                fileOutputStream.write(readBuffer, 0, readLength);
+            }
+
+            s3ObjectInputStream.close();
+            fileOutputStream.close();
+        }
+        catch(AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+        }
+        catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+        catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return file;
+    }
+
+    public void deleteObject(String bucketName, String keyName) {
+        try {
+            s3client.deleteObject(bucketName, keyName);
+        }
+        catch (AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+            System.exit(1);
         }
     }
 
